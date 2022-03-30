512934812c67a49dc33e639fb883ed73ec7b56a2
louisdelperugia <louis.delperugia@unil.ch>
2022-03-30 17:23:21 +0200

    OOP initializer

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
-import pygame
-from sys import exit
+import pygame, sys
+from settings import *
 
-pygame.init()
-screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption('Advanced Programming Project - 2022')
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.clock = pygame.time.Clock()
 
-    pygame.display.update()
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            self.screen.fill("black")
+            pygame.display.update()
+            self.clock.tick(FPS)
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
