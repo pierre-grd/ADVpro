762019395ca5efd8306a6ce26a4b2f6a98800bf7
louisdelperugia <louis.delperugia@unil.ch>
2022-03-30 17:03:57 +0200

    remove error message

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from sys import exit
 
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
@@ -7,5 +8,6 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+            exit()
 
     pygame.display.update()
