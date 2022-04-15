ff74788062785b82ccab2c764ec2ab3a591f63c4
Elodie <79649046+Elodie-kwan@users.noreply.github.com>
2022-04-16 00:42:46 +0200

    moved 2 lines code
    moved 2 lines code in the 'Methods.py' file in their respective function

--- Methods.py
+++ Methods.py
@@ -1,16 +1,18 @@
 import pygame, sys
 from settings import *
 
-def draw_health_point(screen, health_font, health_red, health_blue):
+def draw_health_point(screen, health_red, health_blue):
     # write health point on the screen
+    health_font = pygame.font.SysFont(TEXT_FONT, HEALTH_TEXT_SIZE)
     health_text_red = health_font.render("Health: " + str(health_red), 1, "green")
     health_text_blue = health_font.render("Health: " + str(health_blue), 1, "green")
     screen.blit(health_text_red, (500, 500))
     screen.blit(health_text_blue, (100, 400))
 
 
-def draw_attack_point(screen, attack_point_font, attack_point_red, attack_point_blue):
+def draw_attack_point(screen, attack_point_red, attack_point_blue):
     # write attack point on the screen
+    attack_point_font = pygame.font.SysFont(TEXT_FONT, ATTACK_TEXT_SIZE)
     attack_text_red = attack_point_font.render(
         "Attack point: " + str(attack_point_red), 1, "white")
     attack_text_blue = attack_point_font.render(
--- main.py
+++ main.py
@@ -17,11 +17,9 @@ class Game:
         self.attack_point_box_red = pygame.Rect(500, 150, ATTACK_POINT_BOX_WIDTH, ATTACK_POINT_BOX_HEIGHT)
         self.attack_point_box_blue = pygame.Rect(100, 50, ATTACK_POINT_BOX_WIDTH, ATTACK_POINT_BOX_HEIGHT)
 
-        self.health_font = pygame.font.SysFont(TEXT_FONT, HEALTH_TEXT_SIZE)
         self.health_red = 200
         self.health_blue = 200
 
-        self.attack_point_font = pygame.font.SysFont(TEXT_FONT, ATTACK_TEXT_SIZE)
         self.attack_point_red = 5
         self.attack_point_blue = 5
 
@@ -39,10 +37,10 @@ class Game:
             pygame.draw.rect(self.screen, "blue", self.island_blue)
 
             # write attack point on the screen
-            draw_attack_point(self.screen, self.attack_point_font, self.attack_point_red, self.attack_point_blue)
+            draw_attack_point(self.screen, self.attack_point_red, self.attack_point_blue)
 
             # call 'draw_health_point' function
-            draw_health_point(self.screen, self.health_font, self.health_red, self.health_blue)
+            draw_health_point(self.screen, self.health_red, self.health_blue)
 
             pygame.display.update()
             self.clock.tick(FPS)
