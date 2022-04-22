817d70ab3f07adc2947a1ca862282408e998b505
pierre-grd <79649035+pierre-grd@users.noreply.github.com>
2022-04-22 12:48:04 +0200

    Add os for library management

--- Methods.py
+++ Methods.py
@@ -5,8 +5,8 @@ from settings import *
 def draw_health_point(screen, health_red, health_blue):
     # write health point on the screen
     health_font = pygame.font.SysFont(TEXT_FONT, HEALTH_TEXT_SIZE)
-    health_text_red = health_font.render("Health: " + str(health_red), 1, "green")
-    health_text_blue = health_font.render("Health: " + str(health_blue), 1, "green")
+    health_text_red = health_font.render("Health: " + str(health_red), True, "green")
+    health_text_blue = health_font.render("Health: " + str(health_blue), True, "green")
     screen.blit(health_text_red, (500, 400))
     screen.blit(health_text_blue, (100, 300))
 
@@ -15,9 +15,9 @@ def draw_attack_point(screen, attack_point_red, attack_point_blue):
     # write attack point on the screen
     attack_point_font = pygame.font.SysFont(TEXT_FONT, ATTACK_TEXT_SIZE)
     attack_text_red = attack_point_font.render(
-        "Attack point: " + str(attack_point_red), 1, "white")
+        "Attack point: " + str(attack_point_red), True, "white")
     attack_text_blue = attack_point_font.render(
-        "Attack point: " + str(attack_point_blue), 1, "white")
+        "Attack point: " + str(attack_point_blue), True, "white")
     screen.blit(attack_text_red, (500, 175))
     screen.blit(attack_text_blue, (100, 75))
 
@@ -26,9 +26,9 @@ def draw_defense_point(screen, defense_point_red, defense_point_blue):
     # write attack point on the screen
     defense_point_font = pygame.font.SysFont(TEXT_FONT, DEFENSE_TEXT_SIZE)
     defense_text_red = defense_point_font.render(
-        "Defense point: " + str(defense_point_red), 1, "white")
+        "Defense point: " + str(defense_point_red), True, "white")
     defense_text_blue = defense_point_font.render(
-        "Defense point: " + str(defense_point_blue), 1, "white")
+        "Defense point: " + str(defense_point_blue), True, "white")
     screen.blit(defense_text_red, (500, 155))
     screen.blit(defense_text_blue, (100, 55))
 
@@ -41,6 +41,7 @@ def points(defense_point_red, defense_point_blue, attack_point_red, attack_point
                 defense_point_blue += 2
                 # Pour tester si l'input est prit en compte
                 print("s")
+                print(defense_point_blue)
             elif event.key == pygame.K_DOWN:
                 defense_point_red += 2
             elif event.key == pygame.K_UP:
@@ -53,3 +54,4 @@ def points(defense_point_red, defense_point_blue, attack_point_red, attack_point
                 print("a")
             elif event.key == pygame.K_LEFT:
                 health_point_red += -attack_point_blue
+# ajouter une sortie de boucle a chaque input réalisé
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class Game:
         pygame.init()
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         self.clock = pygame.time.Clock()
-        self.title = pygame.display.set_caption("Advanced Programming Project - 2022")
+        pygame.display.set_caption("Advanced Programming Project - 2022")
 
         self.island_red = pygame.Rect(500, 200, ISLAND_WIDTH, ISLAND_HEIGHT)
         self.island_blue = pygame.Rect(100, 100, ISLAND_WIDTH, ISLAND_HEIGHT)
@@ -42,18 +42,18 @@ class Game:
             draw_attack_point(self.screen, self.attack_point_red, self.attack_point_blue)
 
             # write attack point on the screen
-            draw_defense_point(self.screen, self.defense_point_red, self.defense_point_blue)
+            draw_defense_point(self.screen, self.defense_point_red, DEFENSE_POINT_BLUE)
 
             # call 'draw_health_point' function
             draw_health_point(self.screen, HEALTH_RED, HEALTH_BLUE)
 
             # observe events and acts accordingly(still in crashtest)
-            points(self.defense_point_red, self.defense_point_blue, self.attack_point_red, self.attack_point_blue,
+            points(self.defense_point_red, DEFENSE_POINT_BLUE, self.attack_point_red, self.attack_point_blue,
                    HEALTH_RED, HEALTH_BLUE)
 
-            pygame.display.update()
-
             self.clock.tick(FPS)
+
+            pygame.display.update()
 
 
 if __name__ == '__main__':
